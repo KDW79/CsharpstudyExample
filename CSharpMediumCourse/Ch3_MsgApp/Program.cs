7f3f8244b6a445f77953a4e985c70b9b49ceb0bd
KDW79 <no1holycow@gmail.com>
2020-07-20 16:59:03 +0900

    Ch3 C# 클래스 상속 - 파생 클래스, 추상 클래스, 메서드 오버라이딩 테스트 (이메일 전송 테스트 완)

---
+++ CSharpMediumCourse/Ch3_MsgApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_MsgApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailMessage em = new EmailMessage("holycow@hyundai.com");
+            em.ToWhom = "김동원";
+            em.Message = "테스트가 완료되었습니다.";
+            em.MyDebugMessageInfo();
+            em.Send();
+        }
+    }
+}
