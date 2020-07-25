db9aebcdafc7d78a50c1c52fa595370c907b6b37
Dongwon, Kim <no1holycow@gmail.com>
2020-07-25 23:54:16 +0900

    3장 Thread Safety 실습

--- CSharpAdvancedCourse/CSharpAdvancedCourse/Program.cs
+++ CSharpAdvancedCourse/CSharpAdvancedCourse/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpAdvancedCourse
@@ -10,9 +11,25 @@ namespace CSharpAdvancedCourse
     {
         static void Main(string[] args)
         {
-            new TableMaker().MakeTables();
+            //new TableMaker().MakeTables();
+
+            var tm = new TableMaker();
+
+            Thread[] t = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                t[i] = new Thread(new ThreadStart(tm.DoWork));
+                t[i].Start();
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                t[i].Join();
+            }
+
+            int total = tm.TotalCount;
+            Console.WriteLine($"Total: {total}");
         }
-
-
     }
 }
--- CSharpAdvancedCourse/CSharpAdvancedCourse/TableMaker.cs
+++ CSharpAdvancedCourse/CSharpAdvancedCourse/TableMaker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,35 +10,20 @@ namespace CSharpAdvancedCourse
 {
     class TableMaker
     {
-       public void MakeTables()
-        {
-            DoWork1();
-            DoWork2();
-            //Thread t1 = new Thread(new ThreadStart(DoWork1));
-            //t1.Start();
-            //Thread t2 = new Thread(new ThreadStart(DoWork2));
-            //t2.Start();
-        }
+        private int totalCount = 0;
+        public int TotalCount => totalCount;
+        private object lockObj = new object();
+       //public void MakeTables()
+       // {
+       //     //DoWork1();
+       //     //DoWork2();
+       //     Thread t1 = new Thread(new ThreadStart(DoWork));
+       //     t1.Start();
+       //     Thread t2 = new Thread(new ThreadStart(DoWork));
+       //     t2.Start();
+       // }
 
-        private void DoWork1()
-        {
-            int count = 0;
-            int threadId = Thread.CurrentThread.ManagedThreadId;
-
-            while (count < 10) // 100
-            {
-                // make table
-                // ...
-                Thread.Sleep(1000);
-
-                //if (count == 5) Debugger.Break();
-                count++;
-                Console.WriteLine($"{threadId} : {count}");
-            
-            }
-        }
-
-        private void DoWork2()
+        public void DoWork()
         {
             int count = 0;
             int threadId = Thread.CurrentThread.ManagedThreadId;
@@ -46,13 +32,37 @@ namespace CSharpAdvancedCourse
             {
                 // make table
                 // ...
-                Thread.Sleep(1000);
+                Thread.Sleep(1);
 
-                //if (count == 5) Debugger.Break();
+                //if (count == 5)
+                //    Debugger.Break();
                 count++;
-                Console.WriteLine($"{threadId} : {count}");
+                lock(lockObj)
+                {
+                    totalCount++;
+
+                }
 
             }
+            Console.WriteLine($"{threadId} : {count}");
         }
+
+        //private void DoWork2()
+        //{
+        //    int count = 0;
+        //    int threadId = Thread.CurrentThread.ManagedThreadId;
+
+        //    while (count < 10) // 100
+        //    {
+        //        // make table
+        //        // ...
+        //        Thread.Sleep(1000);
+
+        //        if (count == 3) Debugger.Break();
+        //        count++;
+        //        Console.WriteLine($"{threadId} : {count}");
+
+        //    }
+        //}
     }
 }
