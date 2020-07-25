43c820664d8b67cba1e1ccdb6d33c934db9a2877
Dongwon, Kim <no1holycow@gmail.com>
2020-07-25 12:41:50 +0900

    쓰레드 실습

--- CSharpAdvancedCourse/CSharpAdvancedCourse/TableMaker.cs
+++ CSharpAdvancedCourse/CSharpAdvancedCourse/TableMaker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpAdvancedCourse
@@ -10,7 +11,48 @@ namespace CSharpAdvancedCourse
     {
        public void MakeTables()
         {
-            //DoWork1();
+            DoWork1();
+            DoWork2();
+            //Thread t1 = new Thread(new ThreadStart(DoWork1));
+            //t1.Start();
+            //Thread t2 = new Thread(new ThreadStart(DoWork2));
+            //t2.Start();
+        }
+
+        private void DoWork1()
+        {
+            int count = 0;
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            while (count < 10) // 100
+            {
+                // make table
+                // ...
+                Thread.Sleep(1000);
+
+                //if (count == 5) Debugger.Break();
+                count++;
+                Console.WriteLine($"{threadId} : {count}");
+            
+            }
+        }
+
+        private void DoWork2()
+        {
+            int count = 0;
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            while (count < 10) // 100
+            {
+                // make table
+                // ...
+                Thread.Sleep(1000);
+
+                //if (count == 5) Debugger.Break();
+                count++;
+                Console.WriteLine($"{threadId} : {count}");
+
+            }
         }
     }
 }
