44a411052ce7b8e94d0606140254084df3a2177a
KDW79 <no1holycow@gmail.com>
2020-07-27 10:38:33 +0900

    인터페이스로 객체를 생성하는 실습 COM을 이용하여 생성함

---
+++ CSharpAdvancedCourse/CSharpAdvancedCourse/Ch8_CreateObjFromItf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_CreateObjFromItf
+{
+    using System.Runtime.InteropServices;
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var excel = new Excel.Application();
+            ICar c = new ICar();
+            c.Drive();
+        }
+    }
+
+    [CoClass(typeof(Car)) ]
+    [ComImport]
+    [Guid("510E9CB1-5F91-4DA6-A430-EE0486F8F94C")]
+    interface ICar
+    {
+        void Drive();
+    }
+
+    class Car: ICar
+    {
+        public void Drive()
+        {
+            System.Console.WriteLine("Drive");
+        }
+    }
+}
